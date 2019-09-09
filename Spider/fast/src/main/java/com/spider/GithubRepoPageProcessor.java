f049508f06409d19db5cc3bde2cd1b30b52d699b
yishanchuan <yishanchuan@outlook.com>
2019-09-09 15:03:17 +0800

    add dependence

--- Spider/fast/src/main/java/com/spider/GithubRepoPageProcessor.java
+++ Spider/fast/src/main/java/com/spider/GithubRepoPageProcessor.java
@@ -7,8 +7,8 @@ public class GithubRepoPageProcessor implements PageProcessor {
     // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
     public void process(Page page) {
         // 部分二：定义如何抽取页面信息，并保存下来
-        page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
-        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
+        String author=page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
+        String name=page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
         if (page.getResultItems().get("name") == null) {
             //skip this page
             page.setSkip(true);
@@ -17,6 +17,7 @@ public class GithubRepoPageProcessor implements PageProcessor {
 
         // 部分三：从页面发现后续的url地址来抓取
         page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/[\\w\\-]+/[\\w\\-]+)").all());
+        System.out.println(author+","+name);
     }
 
     @Override
